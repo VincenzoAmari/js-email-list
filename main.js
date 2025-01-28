b5e251750dacc7d1febfc78c54278686555f1383
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-28 16:50:49 +0100

    metodo 1

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-function generateEmails() {
-  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((response) => response.json())
-    .then((data) => {
-      const emailList = document.getElementById("email-list");
-      const newEmail = document.createElement("li");
-      newEmail.textContent = data.response;
-      emailList.appendChild(newEmail);
-    });
-}
+// Variante con il create element
+
+// function generaEmails() {
+//   fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const emailList = document.getElementById("email-list");
+//       const newEmail = document.createElement("li");
+//       newEmail.textContent = data.response;
+//       emailList.appendChild(newEmail);
+//     });
+// }
+
+// function caricaEmails() {
+//   const emailList = document.getElementById("email-list");
+//   emailList.innerHTML = "";
+
+//   for (let i = 0; i < 10; i++) {
+//     generaEmails();
+//   }
+// }
+
+// caricaEmails();
+
+// document
+//   .getElementById("genera-emails")
+//   .addEventListener("click", caricaEmails);
