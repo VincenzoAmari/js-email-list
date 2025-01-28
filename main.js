401d959883a9096789c43ab8d366f6085498de9f
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-28 17:06:09 +0100

    variante con il for

--- main.js
+++ main.js
@@ -25,3 +25,22 @@
 // document
 //   .getElementById("genera-emails")
 //   .addEventListener("click", caricaEmails);
+
+function generaEmails() {
+  const emailList = document.getElementById("email-list");
+  emailList.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => response.json())
+      .then((data) => {
+        emailList.innerHTML += `<li>${data.response}</li>`;
+      });
+  }
+}
+
+generaEmails();
+
+document
+  .getElementById("genera-emails")
+  .addEventListener("click", generaEmails);
